249de06a23950300366e2104971d6afcea80ff5b
grimhang <grimhan2@gmail.com>
2019-12-18 16:26:59 +0900

    로컬머신 제외로직을 case문으로 간단해결

--- 성능/sp_who_chool.sql
+++ 성능/sp_who_chool.sql
@@ -1,25 +1,4 @@
-﻿USE master
-
-IF EXISTS(SELECT 1 FROM sys.tables WHERE NAME = 'TClientConnection') drop table TClientConnection
-CREATE TABLE TClientConnection
-(
-    HostName nvarchar(100) not null Primary key
-    --, UserID nvarchar(100) not null
-    , UserName nvarchar(100) not null
-    , ClientIP nvarchar(100) not null
-    , ClientMac nvarchar(100) not null
- --   , CONSTRAINT PK_TClientConnection PRIMARY KEY CLUSTERED 
-	--(
-	--    HostName
-	--)
-)
-
-use master
-go
-IF EXISTS(SELECT 1 FROM sys.objects WHERE NAME = 'sp_who_chool') drop proc sp_who_chool
-GO
-
-/**************************************************************
+﻿/**************************************************************
 -- Title        : sp_who2의 커스텀 버전
 -- Author 	    : 박성출
 -- Create date  : 2019-09-08
@@ -28,6 +7,7 @@ GO
         DATE         	Developer       Change
         ----------   	--------------- --------------------------
         2019-09-08      박성출         	처음 작성
+        2019-12-18      박성출          client_net_address 에서 <local machine> 제외로직 case문으로 변경
 
         exec sp_who_chool                        -- default(active sesseion)
         exec sp_who_chool 'all'                  -- all sesseion        
@@ -35,7 +15,7 @@ GO
         exec sp_who_chool 'all', 'N'             -- all session, not local
         exec sp_who_chool null, 'N'
 **************************************************************/
-CREATE OR ALTER PROC dbo.sp_who_chool @status varchar(100) = 'active', @IncludeLocalYN CHAR(1) = 'Y', @TYPE TINYINT = 1
+CREATE OR ALTER  PROC dbo.sp_who_chool @status varchar(100) = 'active', @IncludeLocalYN CHAR(1) = 'Y', @TYPE TINYINT = 1
 AS
 BEGIN
     DECLARE @statusVar varchar(100) 
@@ -43,106 +23,63 @@ BEGIN
 
     IF @TYPE = 1
     BEGIN
-        IF @IncludeLocalYN = 'Y'
-        BEGIN
-            SELECT C.session_id
-                , P.open_tran
-                , P.blocked
-                , RTRIM(P.loginame) AS LoginName
-                , DB_Name(P.dbid) DBName
-                --, P.net_address MAC -- 정확치 않음. 확인요망
-                --, rtrim(P.hostname) HostName
-                , ISNULL(CL.HostName, RTRIM(P.hostname)) as Hostname
-                , p.cpu AS CPU
-                , P.physical_io AS DiskIO
-                , CL.UserName
-                --, cl.ClientIP
-                , C.client_net_address AS ClientIP
-                --, CL.ClientMac    
-                , P.net_address ClientMac -- 정확치 않음. 확인요망
-                , RTRIM(
-                    CASE
-                        WHEN P.program_name LIKE 'Microsoft SQL Server Management Studio%' THEN 'SSMS'
-                        WHEN P.program_name LIKE 'SQLAgent%' THEN 'SQL Agent'
-                        WHEN P.program_name LIKE 'Microsoft JDBC Driver for SQL Server%' THEN 'MS JDBC'
-                        ELSE P.program_name
-                    END) AS ClientApp
-                , C.net_transport AS ClientProtocol
-                --, CONVERT(varchar(19), C.connect_time, 121 ) ConnTime
-				, CONVERT(varchar(19), P.last_batch, 121 ) LastBatchTime
-                --,  rtrim(P.lastwaittype) LastWaitType
-                , RTRIM(P.status) Status
-                , P.cmd    
-            FROM sys.dm_exec_connections AS C
-                JOIN sys.sysprocesses AS P                        ON C.session_id = P.spid
-                LEFT join master..TClientConnection AS CL       ON CL.ClientIP = C.client_net_address
-            WHERE 1 = 1
-                --AND RTRIM(P.status) NOT IN ('sleeping')
-                AND (
-                        (
-                            CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)                    ELSE 'a' END
-                            =
-                            CASE WHEN @statusVar = 'active' THEN 'runnable'                        ELSE 'a' END
-                        )
-                        OR 
-                        (
-                            CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)                    ELSE 'a' END
-                            =
-                            CASE WHEN @statusVar = 'active' THEN 'suspended'                        ELSE 'a' END
-                        )
+
+        SELECT C.session_id
+            , P.open_tran
+            , P.blocked
+            , RTRIM(P.loginame) AS LoginName
+            , DB_Name(P.dbid) DBName
+            --, P.net_address MAC -- 정확치 않음. 확인요망
+            --, rtrim(P.hostname) HostName
+            , ISNULL(CL.HostName, RTRIM(P.hostname)) as Hostname
+            , p.cpu AS CPU
+            , P.physical_io AS DiskIO
+            , CL.UserName
+            --, cl.ClientIP
+            , C.client_net_address AS ClientIP
+            --, CL.ClientMac    
+            , P.net_address ClientMac -- 정확치 않음. 확인요망
+            , RTRIM(
+                CASE
+                    WHEN P.program_name LIKE 'Microsoft SQL Server Management Studio%' THEN 'SSMS'
+                    WHEN P.program_name LIKE 'SQLAgent%' THEN 'SQL Agent'
+                    WHEN P.program_name LIKE 'Microsoft JDBC Driver for SQL Server%' THEN 'MS JDBC'
+                    ELSE P.program_name
+                END) AS ClientApp
+            , C.net_transport AS ClientProtocol
+            --, CONVERT(varchar(19), C.connect_time, 121 ) ConnTime
+			, CONVERT(varchar(19), P.last_batch, 121 ) LastBatchTime
+            --,  rtrim(P.lastwaittype) LastWaitType
+            , RTRIM(P.status) Status
+            , P.cmd    
+        FROM sys.dm_exec_connections AS C
+            JOIN sys.sysprocesses AS P                        ON C.session_id = P.spid
+            LEFT join master..TClientConnection AS CL       ON CL.ClientIP = C.client_net_address
+        WHERE 1 = 1
+            AND (
+                    (
+                        CASE WHEN @IncludeLocalYN = 'Y' THEN 'a' ELSE C.client_net_address END
+                        <>
+                        CASE WHEN @IncludeLocalYN = 'Y' THEN 'b' ELSE '<local machine>' END
                     )
-            ORDER BY C.session_id
-        END
-        ELSE
-        BEGIN
-            SELECT C.session_id
-                , P.open_tran
-                , P.blocked
-                , RTRIM(P.loginame) AS LoginName
-                , DB_Name(P.dbid) DBName
-                --, P.net_address MAC -- 정확치 않음. 확인요망
-                --, rtrim(P.hostname) HostName
-                , ISNULL(CL.HostName, RTRIM(P.hostname)) as Hostname
-                , p.cpu AS CPU
-                , P.physical_io AS DiskIO
-                , CL.UserName
-                --, cl.ClientIP
-                , C.client_net_address AS ClientIP
-                --, CL.ClientMac    
-                , P.net_address ClientMac -- 정확치 않음. 확인요망
-                , RTRIM(
-                    CASE
-                        WHEN P.program_name LIKE 'Microsoft SQL Server Management Studio%' THEN 'SSMS'
-                        WHEN P.program_name LIKE 'SQLAgent%' THEN 'SQL Agent'
-                        WHEN P.program_name LIKE 'Microsoft JDBC Driver for SQL Server%' THEN 'MS JDBC'
-                        ELSE P.program_name
-                    END) AS ClientApp
-                , C.net_transport AS ClientProtocol
-                --, CONVERT(varchar(19), C.connect_time, 121 ) ConnTime
-				, CONVERT(varchar(19), P.last_batch, 121 ) LastBatchTime
-                --,  rtrim(P.lastwaittype) LastWaitType
-                , RTRIM(P.status) Status
-                , P.cmd    
-            FROM sys.dm_exec_connections AS C
-                JOIN sys.sysprocesses AS P                        ON C.session_id = P.spid
-                LEFT join master..TClientConnection AS CL       ON CL.ClientIP = C.client_net_address
-            WHERE 1 = 1
-                AND C.client_net_address NOT IN ('<local machine>')
-                --AND RTRIM(P.status) NOT IN ('sleeping')
-                AND (
-                        (
-                            CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)                    ELSE 'b' END
-                            =
-                            CASE WHEN @statusVar = 'active' THEN 'runnable'                        ELSE 'b' END
-                        )
-                        OR 
-                        (
-                            CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)                    ELSE 'b' END
-                            =
-                            CASE WHEN @statusVar = 'active' THEN 'suspended'                        ELSE 'b' END
-                        )
+                )
+            AND (
+                    (
+                        CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)    ELSE 'a' END
+                        =
+                        CASE WHEN @statusVar = 'active' THEN 'runnable'         ELSE 'a' END
                     )
-            ORDER BY C.session_id
-        END
+                    OR 
+                    (
+                        CASE WHEN @statusVar = 'active' THEN RTRIM(P.status)    ELSE 'a' END
+                        =
+                        CASE WHEN @statusVar = 'active' THEN 'suspended'        ELSE 'a' END
+                    )
+                )
+        ORDER BY C.session_id
+
     END
 END
+
+
+
