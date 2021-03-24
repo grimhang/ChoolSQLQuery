e060033f2439e4dc1a7d0921f46974eceb0c467b
Sungchool Park <grimhan2@gmail.com>
2021-03-24 13:55:10 +0900

    파워쉘추가

--- 시스템/CPU_Memory.sql
+++ 시스템/CPU_Memory.sql
@@ -1,10 +1,10 @@
--- Ω√Ω∫≈€¡∂ªÁ(CPU, Memory)
+-- ÏãúÏä§ÌÖúÏ°∞ÏÇ¨(CPU, Memory)
 SELECT cpu_count AS LogiclaCPUCount, hyperthread_ratio AS HyperthreadRatio, cpu_count / hyperthread_ratio AS PhysicalCPUCount
 	, affinity_type, affinity_type_desc, virtual_machine_type, virtual_machine_type_desc
 	, physical_memory_kb / 1024 physical_memory_MB, sql_memory_model, sql_memory_model_desc
 FROM sys.dm_os_sys_INFO
 
--- Memory ¡∂ªÁ
+-- Memory Ï°∞ÏÇ¨
 select total_physical_memory_kb / 1024		AS total_physical_memory_MB
 	, available_physical_memory_kb / 1024	AS available_physical_memory_MB
 	, total_page_file_kb / 1024				AS total_page_file_MB
@@ -17,9 +17,9 @@ select total_physical_memory_kb / 1024		AS total_physical_memory_MB
 	, system_memory_state_desc
 from sys.dm_os_sys_memory
 
--- SQL Server ø£¡¯ Process ¡§∫∏
+-- SQL Server ÏóîÏßÑ Process Ï†ïÎ≥¥
 SELECT
-	physical_memory_in_use_kb / 1024		AS physical_memory_in_use_MB	-- SQL Serverø°º≠ ¬˜¡ˆ«œ∞Ì ¿÷¥¬ ∏ﬁ∏∏Æ
+	physical_memory_in_use_kb / 1024		AS physical_memory_in_use_MB	-- SQL ServerÏóêÏÑú Ï∞®ÏßÄÌïòÍ≥† ÏûàÎäî Î©îÎ™®Î¶¨
 	, large_page_allocations_kb / 1024		AS large_page_allocations_MB
 	, locked_page_allocations_kb / 1024		AS locked_page_allocations_MB
 	, total_virtual_address_space_kb, virtual_address_space_reserved_kb, virtual_address_space_committed_kb, virtual_address_space_available_kb, page_fault_count, memory_utilization_percentage, available_commit_limit_kb, process_physical_memory_low, process_virtual_memory_low
@@ -27,7 +27,7 @@ FROM sys.dm_os_process_memory
 
 
 
--- µ•¿Ã≈Õ∫£¿ÃΩ∫∫∞ Buffer Pool ¡§∫∏
+-- Îç∞Ïù¥ÌÑ∞Î≤†Ïù¥Ïä§Î≥Ñ Buffer Pool Ï†ïÎ≥¥
 select db_name(database_id) as DB
 	, Convert(decimal(20,0), count(row_count) * 8.00 / 1024.00) AS MB
 	, Convert(decimal(10,2), count(row_count) * 8.00 / 1024.00 / 1024.00) AS GB
@@ -38,3 +38,6 @@ order by db_name(database_id)
 --select cntr_value / 1024 / 1024.00
 --from sys.dm_os_performance_counters
 --where counter_name in ('Total Server Memory (KB)', 'Target Server Memory (KB)');
+
+--- powershell
+ps sqlservr | ft name,  @{name='PrivateMemory(MB)';expression={$_.PrivateMemorySize64/1MB};formatstring='N0';align='right'}, @{name='WorkingSet(MB)';expression={$_.Workingset64/1MB};formatstring='N0';align='right'} -AutoSize
