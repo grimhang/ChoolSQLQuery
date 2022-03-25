f837ee57b9aaf75d772b8a3db9540e51f9626293
Sungchool Park <grimhan2@gmail.com>
2022-03-25 20:45:58 +0900

    Update sp_who_chool.sql
    클라이언트 접속 정보 테이블 제거한 심플버전

--- 성능/sp_who_chool.sql
+++ 성능/sp_who_chool.sql
@@ -1,7 +1,17 @@
-﻿/**************************************************************
--- Title        : sp_who2의 커스텀 버전
--- Author 	    : 박성출
+use master
+go
+
+IF OBJECT_ID('sp_who_chool') IS NULL
+    EXEC ('CREATE PROC sp_who_chool AS SELECT 1')
+GO
+
+ALTER  PROC dbo.sp_who_chool @status varchar(100) = 'active', @IncludeLocalYN CHAR(1) = 'Y', @TYPE TINYINT = 1
+
+/**************************************************************
+-- Title        : sp_who2의 커스텀 버전 V1
+-- Author 	: 박성출
 -- Create date  : 2019-09-08
+-- Alter date   : 2022-03-25
 -- Description  : 나만의 버전
     
         DATE         	Developer       Change
@@ -15,7 +25,8 @@
         exec sp_who_chool 'all', 'N'             -- all session, not local
         exec sp_who_chool null, 'N'
 **************************************************************/
-CREATE OR ALTER  PROC dbo.sp_who_chool @status varchar(100) = 'active', @IncludeLocalYN CHAR(1) = 'Y', @TYPE TINYINT = 1
+
+
 AS
 BEGIN
     DECLARE @statusVar varchar(100) 
@@ -31,10 +42,10 @@ BEGIN
             , DB_Name(P.dbid) DBName
             --, P.net_address MAC -- 정확치 않음. 확인요망
             --, rtrim(P.hostname) HostName
-            , ISNULL(CL.HostName, RTRIM(P.hostname)) as Hostname
+            --, ISNULL(CL.HostName, RTRIM(P.hostname)) as Hostname
             , p.cpu AS CPU
             , P.physical_io AS DiskIO
-            , CL.UserName
+            --, CL.UserName
             --, cl.ClientIP
             , C.client_net_address AS ClientIP
             --, CL.ClientMac    
@@ -54,7 +65,7 @@ BEGIN
             , P.cmd    
         FROM sys.dm_exec_connections AS C
             JOIN sys.sysprocesses AS P                        ON C.session_id = P.spid
-            LEFT join master..TClientConnection AS CL       ON CL.ClientIP = C.client_net_address
+            -- LEFT join master..TClientConnection AS CL       ON CL.ClientIP = C.client_net_address
         WHERE 1 = 1
             AND (
                     (
@@ -82,4 +93,6 @@ BEGIN
 END
 
 
-
+EXEC sys.sp_MS_marksystemobject
+    'sp_who_chool';   -- skip this for Azure
+GO    
