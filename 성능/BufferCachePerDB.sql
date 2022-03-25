b1ca2c0270c0d2607e8ac7606a9cfcda3037b171
Sungchool Park <grimhan2@gmail.com>
2022-03-25 10:26:39 +0900

    Update BufferCachePerDB.sql
    2022-03-25 change

--- 성능/BufferCachePerDB.sql
+++ 성능/BufferCachePerDB.sql
@@ -1,26 +1,28 @@
 -- Note: querying sys.dm_os_buffer_descriptors
 -- requires the VIEW_SERVER_STATE permission.
-DECLARE @total_buffer INT;
-SELECT @total_buffer = cntr_value
-FROM sys.dm_os_performance_counters
-WHERE RTRIM([object_name]) LIKE '%Buffer Manager'
-	AND counter_name = 'Total Pages';;
-WITH src
-AS (
-	SELECT database_id
-		,db_buffer_pages = COUNT_BIG(*)
-	FROM sys.dm_os_buffer_descriptors
-	--WHERE database_id BETWEEN 5 AND 32766
-	GROUP BY database_id
-	)
-SELECT [db_name] = CASE [database_id]
-		WHEN 32767
-			THEN 'Resource DB'
-		ELSE DB_NAME([database_id])
-		END
-	,db_buffer_pages
-	,db_buffer_MB = db_buffer_pages / 128
-	,db_buffer_percent = CONVERT(DECIMAL(6, 3), db_buffer_pages * 100.0 / @total_buffer)
-FROM src
-ORDER BY db_buffer_MB DESC;
-
+SELECT bf.DatabaseName    
+    , bf.DirtyPageCount AS "DirtyPageCount (a)"
+    , bf.CleanPageCount AS "CleanPageCount (b)"
+    , (bf.DirtyPageCount + bf.CleanPageCount) AS "TotalPagesCount (a+b)"
+    , '---' AS Devider
+    , [DirtyPageCount] * 8 / 1024 AS [DirtyPageMB (c)]
+    , [CleanPageCount] * 8 / 1024 AS [CleanPageMB (d)]
+    , ([DirtyPageCount] + [CleanPageCount]) * 8 / 1024 AS "TotalPagesMB (c+d)"
+FROM
+(
+    SELECT    
+        CASE 
+                WHEN ([database_id] = 32767)                    THEN N'Resource Database'
+                ELSE DB_NAME([database_id])
+        END AS [DatabaseName]
+        , SUM(CASE                 WHEN ([is_modified] = 1)                    THEN 1
+                ELSE 0
+                END) AS [DirtyPageCount]
+        , SUM(CASE                 WHEN ([is_modified] = 1)                    THEN 0
+                ELSE 1
+                END) AS [CleanPageCount]
+    FROM sys.dm_os_buffer_descriptors
+    GROUP BY [database_id]
+) AS [bf]
+ORDER BY [DatabaseName]
+GO
